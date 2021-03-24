b3e758506841e3b412f5de0c420eb359f5c973ad
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2021-03-24 12:04:48 +0200

    Add files via upload

---
+++ 05. LINQ/MusicHub/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicHub.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=DESKTOP-3PBD2BD\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True";
+    }
+}
