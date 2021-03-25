f20ec0f85a673e3d117d1865b60022085d79f3cf
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2021-03-25 15:53:31 +0200

    Add files via upload

---
+++ 06. Advanced-Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString 
+            => @"Server=DESKTOP-3PBD2BD\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
