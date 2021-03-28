e817586bf201362442aab65a683637bb1e2c0f99
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2021-03-28 17:03:12 +0300

    Add files via upload

---
+++ 07. Auto-Mapping-Objects/FastFood.Data/FastFoodContextDesignTimeFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FastFood.Data
+{
+    public class FastFoodContextDesignTimeFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<FastFoodContext>();
+
+            builder.UseSqlServer("Server=DESKTOP-3PBD2BD\\SQLEXPRESS;Database=FastFood;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new FastFoodContext(builder.Options);
+        }
+    }
+}
