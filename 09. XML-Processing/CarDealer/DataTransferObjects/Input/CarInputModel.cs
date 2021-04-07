681f474dd38bb05687a44afa34d9891b9b0b2afc
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2021-04-07 20:19:20 +0300

    Add files via upload

---
+++ 09. XML-Processing/CarDealer/DataTransferObjects/Input/CarInputModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObjects.Input
+{
+    [XmlType("Car")]
+    public class CarInputModel
+    {
+        [XmlElement("make")]
+        public string Make { get; set; }
+
+        [XmlElement("model")]
+        public string Model { get; set; }
+
+        [XmlElement("TraveledDistance")]
+        public long TraveledDistance { get; set; }
+
+        [XmlArray("parts")]
+        public CarPartsInputModel[] CarPartsInputModel { get; set; }
+    }
+}
---
+++ 09. XML-Processing/CarDealer/DataTransferObjects/Input/SaleInputModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObjects.Input
+{
+    [XmlType("Sale")]
+    public class SaleInputModel
+    {
+        [XmlElement("discount")]
+        public decimal Discount { get; set; }
+
+        [XmlElement("carId")]
+        public int CarId { get; set; }
+
+        [XmlElement("customerId")]
+        public int CustomerId { get; set; }
+    }
+}
