581e3fe730bb2cbe87e743a52e3bc6e06a90e4e5
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2021-04-08 19:08:13 +0300

    Add files via upload

---
+++ Exams/Exam - 08 August 2020/VaporStore/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace VaporStore.Data
+{
+	public static class Configuration
+	{
+		public static string ConnectionString =
+			@"Server=DESKTOP-3PBD2BD\SQLEXPRESS;Database=VaporStore;Trusted_Connection=True";
+	}
+}
