98e486f5de37538e901a3fcae2e5df3d2931964e
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2021-04-14 17:02:15 +0300

    Add files via upload

---
+++ Exams/Exam - 04 April 2021/TeisterMask/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace TeisterMask.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-3PBD2BD\SQLEXPRESS;Database=TeisterMask;Trusted_Connection=True";
+    }
+}
---
+++ Exams/Exam - 04 April 2021/TeisterMask/Data/Models/Project.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TeisterMask.Data.Models
+{
+    public class Project
+    {
+        public Project()
+        {
+            this.Tasks = new HashSet<Task>();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        public DateTime OpenDate { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
